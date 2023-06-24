c023afdd5096d357196ee7487b359c46a9b4a1bd
Lump1 <mayner366@gmail.com>
2023-06-24 03:07:13 +0300

    Added styles for menu

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -7,14 +7,23 @@ export default function Menu(props) {
   return (
     <div className='menu-container'>
         <h2 className='date-menu'>Date: </h2>
+        <hr className='menu-hr' />
         <p className='day'>Day: {props.date.getDay()}</p>
         <p className='month'>Month: {props.date.toLocaleString('en', { month: 'long' })}</p>
         <p className='year'>Year: {props.date.getFullYear()}</p>
+
+        <hr className='menu-hr' />
         
-        <h2 className='m-10'>Month</h2>
+        <h2 className='options-text'>Month</h2>
         <div className='button-div'>
-            <button className=''>←</button>
-            <button>→</button>
+            <button className='btn-arrow'>←</button>
+            <button className='btn-arrow'>→</button>
+        </div>
+
+        <h2 className='options-text'>Year</h2>
+        <div className='button-div'>
+            <button className='btn-arrow'>←</button>
+            <button className='btn-arrow'>→</button>
         </div>
         
     </div>
